0b202772b753e2e8df346838a1a19a351cfcf9f5
nikolame1991 <nikolamedan1991@gmail.com>
2025-06-19 19:42:41 +0200

    Update TestDataContact.java

--- TestDataContact.java
+++ TestDataContact.java
@@ -4,15 +4,28 @@ import org.testng.annotations.DataProvider;
 
 public class TestDataContact {
 
+	// Validno ime za testiranje kontakt forme
+	// Valid first name for contact form testing
 	public String VALID_NAMECONTACT = "Nikola";
+
+	// Validno prezime za testiranje kontakt forme
+	// Valid last name for contact form testing
 	public String VALID_LASTNAMECONTACT = "Medan";
+
+	// Validna email adresa za testiranje
+	// Valid email address for testing
 	public String VALID_EMAIL = "nikolamedan1991@gmail.com";
+
+	// Validna poruka za tekstualno polje
+	// Valid message for textarea input
 	public String VALID_TEXTAREA = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
 
+	// TestNG DataProvider koji vraća kombinaciju korisničkih podataka
+	// TestNG DataProvider returning a combination of user input data
 	@DataProvider(name = "userData")
 	public Object[][] userData() {
-		return new Object[][] { { "Nikola", "Medan", "nikolamedan1991@gmail.com",
-				"Lorem Ipsum is simply dummy text of the printing and typesetting industry." } };
+		return new Object[][] {
+			{ VALID_NAMECONTACT, VALID_LASTNAMECONTACT, VALID_EMAIL, VALID_TEXTAREA }
+		};
 	}
-
 }
