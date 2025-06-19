9011a4b1e19c5f1b9efb76e2cdf2eaab55a957cd
nikolame1991 <nikolamedan1991@gmail.com>
2025-06-19 19:37:35 +0200

    Update PageClasses.java

--- PageClasses.java
+++ PageClasses.java
@@ -15,73 +15,77 @@ public class PageClasses {
 	WebDriver driver;
 	WebDriverWait wait;
 
-	// Constructor // Konstruktor
+	// Konstruktor klase – inicijalizuje WebDriver i WebDriverWait
+	// Class constructor – initializes WebDriver and WebDriverWait
 	public PageClasses(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 
-	// Web elements using @FindBy annotation // Web elementi koristeći @FindBy
-	// anotaciju
+	// Web elementi korišćeni pomoću @FindBy anotacija
+	// Web elements using @FindBy annotation
+
 	@FindBy(css = "#site-title > a")
-	WebElement clickLink;
+	WebElement clickLink; // Logo sajta / Site logo
 
 	@FindBy(css = "#menu-item-20")
-	WebElement clickNavPractice;
+	WebElement clickNavPractice; // Navigacija do Practice strane / Navigation to Practice page
 
 	@FindBy(css = "#loop-container > div > article > div.post-content > div:nth-child(2) > div:nth-child(1) > p > a")
-	WebElement clickTestLoginPage;
+	WebElement clickTestLoginPage; // Link ka Test Login stranici / Link to Test Login page
 
 	@FindBy(css = "#login > h2")
-	WebElement checkHeading;
+	WebElement checkHeading; // Naslov na login stranici / Heading on login page
 
 	@FindBy(css = "#login > ul > li:nth-child(1)")
-	WebElement checkText;
+	WebElement checkText; // Tekst ispod naslova / Text under the heading
 
 	@FindBy(css = "#username")
-	WebElement inputFieldUserName;
+	WebElement inputFieldUserName; // Polje za korisničko ime / Username input field
 
 	@FindBy(css = "#password")
-	WebElement inputFieldPassword;
+	WebElement inputFieldPassword; // Polje za lozinku / Password input field
 
 	@FindBy(css = "#submit")
-	WebElement clickButton;
+	WebElement clickButton; // Dugme za prijavu / Submit (login) button
 
 	@FindBy(xpath = "//*[@id=\"loop-container\"]/div/article/div[2]/div/div/div/a")
-	WebElement clickButtonLogOut;
+	WebElement clickButtonLogOut; // Dugme za odjavu / Logout button
 
 	@FindBy(xpath = "//*[@id=\"error\"]")
-	WebElement showError;
+	WebElement showError; // Poruka o grešci / Error message
 
 	@FindBy(xpath = "//*[@id=\"login\"]/hr[1]")
-	WebElement horizontalRule;
+	WebElement horizontalRule; // Horizontalna linija / Horizontal line
 
 	@FindBy(css = "#login > h5:nth-child(6)")
-	WebElement header5Displayed;
+	WebElement header5Displayed; // Podnaslov test slučaja / Test case subtitle
 
 	@FindBy(css = "#login > ol:nth-child(7)")
-	WebElement informationDisplay;
+	WebElement informationDisplay; // Informacije o test koracima / Test steps info
 
 	@FindBy(css = "#menu-item-18 > a")
-	WebElement clickContactLink;
+	WebElement clickContactLink; // Link ka kontakt stranici / Link to contact page
 
-	// Methods to interact with web elements // Metodi za interakciju sa web
-	// elementima
+	// ==== Metodi za interakciju sa elementima / Methods for interacting with elements ====
+
+	// Klik na logo / Click on site logo
 	public void clickLinkLogo() {
 		wait.until(ExpectedConditions.elementToBeClickable(clickLink)).click();
 	}
 
+	// Klik na navigaciju "Practice" / Click on "Practice" navigation
 	public void clickPracticeNav() {
 		wait.until(ExpectedConditions.elementToBeClickable(clickNavPractice)).click();
 	}
 
+	// Klik na link za test login stranicu / Click on test login page link
 	public void verifyClickTestLoginPage() {
 		wait.until(ExpectedConditions.elementToBeClickable(clickTestLoginPage)).click();
 	}
 
-	// Verifies that the login page heading is correct // Proverava da li je naslov
-	// stranice za prijavu ispravan
+	// Provera naslova stranice / Verify page heading
 	public void verifyHeading() {
 		WebElement headingElement = wait.until(ExpectedConditions.visibilityOf(checkHeading));
 		String actualHeading = headingElement.getText();
@@ -89,8 +93,7 @@ public class PageClasses {
 		Assert.assertEquals(actualHeading, expectedHeading);
 	}
 
-	// Checks if the expected login text is displayed // Proverava da li se
-	// očekivani tekst za prijavu prikazuje
+	// Proverava da li se tekst ispod naslova prikazuje / Check if text under heading is displayed
 	public void verifyText() {
 		WebElement textElement = wait.until(ExpectedConditions.visibilityOf(checkText));
 		String actualText = textElement.getText();
@@ -98,51 +101,47 @@ public class PageClasses {
 		Assert.assertTrue(actualText.contains(expectedText));
 	}
 
-	// Enters username into the input field // Unosi korisničko ime u polje za unos
+	// Unos korisničkog imena / Enter username
 	public void fieldInputName(String username) {
 		wait.until(ExpectedConditions.visibilityOf(inputFieldUserName));
 		inputFieldUserName.sendKeys(username);
 	}
 
-	// Enters password into the input field // Unosi lozinku u polje za unos
+	// Unos lozinke / Enter password
 	public void fieldInputPassword(String password) {
 		wait.until(ExpectedConditions.visibilityOf(inputFieldPassword));
 		inputFieldPassword.sendKeys(password);
 	}
 
-	// Clicks the submit button to log in // Klikne na dugme za prijavu
+	// Klik na dugme za prijavu / Click submit button
 	public void clickButtonSubmit() {
 		wait.until(ExpectedConditions.elementToBeClickable(clickButton)).click();
 	}
 
-	// Checks if the logout button is available // Proverava da li je dugme za
-	// odjavu dostupno
+	// Proverava da li je dugme za odjavu prikazano / Check if logout button is present
 	public void clickButtonLogOut() {
 		if (driver.findElements(By.xpath("//*[@id='loop-container']/div/article/div[2]/div/div/div/a")).size() > 0) {
-			System.out.println("Element je pronađen!"); // Element found!
+			System.out.println("Element je pronađen! / Element found!");
 		} else {
-			System.out.println("Element ne postoji u DOM-u!"); // Element does not exist in DOM!
+			System.out.println("Element ne postoji u DOM-u! / Element not present in DOM!");
 		}
 	}
 
-	// Checks if the error message is displayed // Proverava da li se prikazuje
-	// poruka o grešci
+	// Proverava da li se prikazuje poruka o grešci / Check if error message is displayed
 	public void showError() {
 		if (driver.findElements(By.xpath("//*[@id='error']")).size() > 0) {
-			System.out.println("Element je pronađen!");
+			System.out.println("Element je pronađen! / Element found!");
 		} else {
-			System.out.println("Element ne postoji u DOM-u!");
+			System.out.println("Element ne postoji u DOM-u! / Element not present in DOM!");
 		}
 	}
 
-	// Checks if the horizontal rule is visible // Proverava da li je horizontalna
-	// linija vidljiva
+	// Proverava da li je horizontalna linija vidljiva / Check if horizontal line is visible
 	public boolean isHorizontalRuleDisplayed() {
 		return horizontalRule.isDisplayed();
 	}
 
-	// Verifies another login page text // Proverava drugi tekst na stranici za
-	// prijavu
+	// Alternativna metoda za proveru teksta (može se refaktorisati) / Duplicate of verifyText (can be refactored)
 	public void verifyText1() {
 		WebElement textElement = wait.until(ExpectedConditions.visibilityOf(checkText));
 		String actualText = textElement.getText();
@@ -150,7 +149,7 @@ public class PageClasses {
 		Assert.assertTrue(actualText.contains(expectedText));
 	}
 
-	// Checks if the header is displayed // Proverava da li je zaglavlje prikazano
+	// Proverava prikaz podnaslova (npr. Test case 1) / Check if H5 header is visible (e.g. test case title)
 	public void isHeader5Displayed() {
 		WebElement textHeader5 = wait.until(ExpectedConditions.visibilityOf(header5Displayed));
 		String actualTextHeader5 = textHeader5.getText();
@@ -158,15 +157,13 @@ public class PageClasses {
 		Assert.assertEquals(actualTextHeader5, expectedTextHeader5);
 	}
 
-	// Checks if informational text is displayed // Proverava da li je informativni
-	// tekst prikazan
+	// Proverava da li su prikazane informacije o testu / Check if test info steps are displayed
 	public boolean isInformationDisplayed() {
 		return informationDisplay.isDisplayed();
 	}
 
-	// Clicks the contact link // Klikne na kontakt link
+	// Klik na kontakt link / Click on contact link
 	public void linkContactClick() {
 		wait.until(ExpectedConditions.elementToBeClickable(clickContactLink)).click();
 	}
-
 }
