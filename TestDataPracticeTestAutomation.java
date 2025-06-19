bea7d5677b6561826c265b3fa17905aad3ccf513
nikolame1991 <nikolamedan1991@gmail.com>
2025-06-19 19:47:08 +0200

    Update TestDataPracticeTestAutomation.java

--- TestDataPracticeTestAutomation.java
+++ TestDataPracticeTestAutomation.java
@@ -4,11 +4,20 @@ import org.testng.annotations.DataProvider;
 
 public class TestDataPracticeTestAutomation {
 
+	// Validno korisničko ime za test login stranice
+	// Valid username for test login page
 	public String VALID_NAME = "student";
+
+	// Validna lozinka za test login stranice
+	// Valid password for test login page
 	public String VALID_PASSWORD = "Password123";
 
+	// TestNG DataProvider koji obezbeđuje podatke za testiranje logovanja
+	// TestNG DataProvider providing login credentials for testing
 	@DataProvider(name = "userData")
 	public Object[][] userData() {
-		return new Object[][] { { "student", "Password123" } };
+		return new Object[][] {
+			{ VALID_NAME, VALID_PASSWORD }
+		};
 	}
 }
